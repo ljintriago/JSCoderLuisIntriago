585a311e1d87bb6a76cd6ce35ac5022d0ad57668
Luis Intriago <ljintriago@hotmail.com>
2023-07-06 11:04:09 -0500

    Correcciones segunda [reentrega

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const impuesto = 0.12;
 
 btnCalcular.onclick = () =>{handleClick()};
 
+let cliente = {
+    nommbre: '',
+    apellido: '',
+    edad: 0,
+    mail: '',
+    nombrePerfil: ''
+}
+
 function handleClick(){
     let monto = parseFloat(document.getElementById("monto").value);
     let plazo = parseInt(document.getElementById("plazo").value);
@@ -10,29 +18,56 @@ function handleClick(){
     alert(calcular(monto, plazo, impuesto));
 }
 
-function calcular(m, p, i){
+function calcular(monto, plazo, impuesto){
     let resultado = 0;
     let imp = 0;
     let impTotal = 0;
     let cuota = 0;
-    if (m===0 || p===0){
+    if (monto===0 || p===0){
         return "No se upede calcular la cuota si esta vacio uno de los parametros";
     }
     else{
-        cuota = m / p;
+        cuota = monto / plazo;
 
-        for(x = 1; x <= p; x++){
+        for(x = 1; x <= plazo; x++){
             
-            if (x > 1 && x < p) {
-                m = m - cuota;
+            if (x > 1 && x < plazo) {
+                monto = monto - cuota;
             }
            
-            imp = m * i; 
+            imp = monto * impuesto; 
 
             impTotal = impTotal + imp;
         }
         
-        resultado = m + impTotal;
-        return `La cuota a pagar es ${cuota} con impuesto a anual de ${i*100}% y el total es ${resultado}`;
+        resultado = monto + impTotal;
+        return `La cuota a pagar es ${cuota} con impuesto a anual de ${impuesto*100}% y el total es ${resultado}`;
     }
 }
+
+function setInitialInfo(obj, nombre, apellido, edad, mail){
+    obj.nombre = nombre;
+    obj.apellido = apellido;
+    obj.edad = edad;
+    obj.mail = mail;
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); 
+}
+
+function setProfileName(obj){
+    let profName = [];
+    profName.push(obj.nombre[0].substring(0,2));
+    profName.push(obj.apellido[0].substring(0,2));
+    profName.push(getRandomInt(1000, 10000));
+
+    return profName.toString();
+}
+
+
+setInitialInfo(cliente, "Luis", "Intriago", "27", "any@abcd.com");
+
+cliente.nombrePerfil = setProfileName(cliente);
