6482f6cd462c8685d566e1d9db80788b8dca099c
Luis Intriago <ljintriago@hotmail.com>
2023-08-17 14:25:27 -0500

    Correccion reentrega proyecto final

--- js/index.js
+++ js/index.js
@@ -1,49 +1,6 @@
-/* const impuesto = 0.12;
+let DateTime = luxon.DateTime;
+let Interval = luxon.Interval;
 
-btnCalcular.onclick = () =>{handleClick()};
-
-let cliente = {
-    nommbre: '',
-    apellido: '',
-    edad: 0,
-    mail: '',
-    nombrePerfil: ''
-}
-
-function handleClick(){
-    let monto = parseFloat(document.getElementById("monto").value);
-    let plazo = parseInt(document.getElementById("plazo").value);
-    let btnCalcular = document.getElementById("btnCalcular");
-
-    alert(calcular(monto, plazo, impuesto));
-}
-
-function calcular(monto, plazo, impuesto){
-    let resultado = 0;
-    let imp = 0;
-    let impTotal = 0;
-    let cuota = 0;
-    if (monto===0 || p===0){
-        return "No se upede calcular la cuota si esta vacio uno de los parametros";
-    }
-    else{
-        cuota = monto / plazo;
-
-        for(x = 1; x <= plazo; x++){
-            
-            if (x > 1 && x < plazo) {
-                monto = monto - cuota;
-            }
-           
-            imp = monto * impuesto; 
-
-            impTotal = impTotal + imp;
-        }
-        
-        resultado = monto + impTotal;
-        return `La cuota a pagar es ${cuota} con impuesto a anual de ${impuesto*100}% y el total es ${resultado}`;
-    }
-} */
 let usrName;
 let usrDir;
 let usrTel;
@@ -51,21 +8,44 @@ let usrProCita;
 let petName;
 let petAge;
 let petRace;
+let infolink = document.getElementById("linkInfo");
+let infolink2 = document.getElementById("linkInfo2");
+let btndog1 = document.getElementById("btn-dog-1");
+let btndog2 = document.getElementById("btn-dog-2");
+   
+
+infolink.onclick = () =>{handleInfo()};
+infolink2.onclick = () =>{handleInfo()};
+
+btndog1.onclick = () =>{setNextAppointment()}
+btndog2.onclick = () =>{setNextAppointment2()}
 
 fetch('./json/mascotas.json')
     .then((response) => response.json())
     .then((json) => {setCustomerAndPetInitialInfo(json)});
    
 
-function setCustomerAndPetInitialInfo(obj){
+async function setCustomerAndPetInitialInfo(obj){
+    let customer;
+    let proxcita;
+    
+    if(localStorage.getItem('client') === undefined){
+        localStorage.setItem('client', obj)
+        customer = localStorage.getItem('client')
+    }
+    else{
+        customer = localStorage.getItem('client')
+    }
+
     usrName = document.getElementById("nom-usuario");
     usrDir = document.getElementById("dir");
     usrTel = document.getElementById("tel");
-    usrProCita = document.getElementById("pro-cita");
+    usrProCita = document.getElementById("prox-cita");
 
     usrName.innerText = usrName.innerText + " " + obj.nombre
     usrDir.innerText = obj.dir
     usrTel.innerText = obj.tel
+    
 
     for(i=1; i <=2; i++){
         petName = document.getElementById("nombre"+i);
@@ -74,45 +54,128 @@ function setCustomerAndPetInitialInfo(obj){
 
         petName.innerText = petName.innerText + " " + obj.mascotas[i].nombre;
         petAge.innerText = petAge.innerText + " " + obj.mascotas[i].edad + " años";
-        petRace.innerText = petRace.innerText + " " + getDogBrand(obj.mascotas[i].raza);
+        let race = await getDogBrand(obj.mascotas[i].raza);
+        petRace.innerText = petRace.innerText + " " + race.name;
+
+        if(typeof obj.mascotas[i+1] === "undefined" && proxcita === null){
+            proxcita = obj.mascotas[i].proximaCita
+        }
+        else{
+            let currenDate = DateTime.now();
+            let d1 = DateTime.local(obj.mascotas[i].proximaCita.substring(4),obj.mascotas[i].proximaCita.substring(2,4),obj.mascotas[i].proximaCita.substring(0,2));
+            let i1 = Interval.fromDateTimes(currenDate, d1);
+
+            let d2 = DateTime.local(obj.mascotas[2].proximaCita.substring(4),obj.mascotas[2].proximaCita.substring(2,4),obj.mascotas[2].proximaCita.substring(0,2));
+            let i2 = Interval.fromDateTimes(currenDate, d2);
+
+            if(i1===i2){
+                proxcita = obj.mascotas[i].proximaCita
+            }
+            else if(i1<i2){
+                proxcita = obj.mascotas[i].proximaCita
+            }
+            else{
+                proxcita = obj.mascotas[2].proximaCita
+            }
+
+        }
+        
     }
     
+    if (proxcita !== null){
+        usrProCita.innerText = usrProCita.innerText + " " + DateTime.local(parseInt(proxcita.substring(4)), parseInt(proxcita.substring(2,4)), parseInt(proxcita.substring(0,2))).toString();
+    }
     
 }
 
 
 async function getDogBrand(id){
-    await fetch('https://api.thedogapi.com/v1/breeds/'+id)
-    .then((response) => response.json())
-    .then((json) => {
-        return json.name
-    })
+   try{
+    const response = await fetch('https://api.thedogapi.com/v1/breeds/'+id);
+    const data = await response.json();
+    return data
+   }catch{
+    console.log("Error")
+   }
+
 }
 
 
-/* function setInitialInfo(obj, nombre, apellido, edad, mail){
-    obj.nombre = nombre;
-    obj.apellido = apellido;
-    obj.edad = edad;
-    obj.mail = mail;
-}
+async function handleInfo(){
+    let race = await getDogBrand(193);
+    let resultText = `Peso normal: ${race.weight.metric} Kg \nAltura normal: ${race.height.metric} cm \nTemperamento: ${race.temperament} \nGrupo de raza: ${race.breed_group} \nEsperanza de vida: ${race.life_span}`;
 
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min); 
-}
-
-function setProfileName(obj){
-    let profName = [];
-    profName.push(obj.nombre[0].substring(0,2));
-    profName.push(obj.apellido[0].substring(0,2));
-    profName.push(getRandomInt(1000, 10000));
-
-    return profName.toString();
+    Swal.fire({
+        title: 'Información',
+        text: resultText,
+        icon: 'info',
+        confirmButtonText: 'Cool'
+      });
 }
 
 
-setInitialInfo(cliente, "Luis", "Intriago", "27", "any@abcd.com");
+function setNextAppointment(){
+    Swal.fire({
+        title: 'Entrarla fecha en el formato DDMMYYYY',
+        input: 'text',
+        inputAttributes: {
+          autocapitalize: 'off'
+        },
+        showCancelButton: true,
+        confirmButtonText: 'Confirmar',
+        showLoaderOnConfirm: true,
+        preConfirm: (date) => {
+          try{
+            let customer = localStorage.getItem('client');
+            customer.mascotas[1].proximaCita = date;
+            localStorage.setItem('client', customer);
+            console.log(customer)
+          } 
+        catch{
+              Swal.showValidationMessage(
+                `Fallo al agendar`
+              )
+            }
+        },
+        allowOutsideClick: () => !Swal.isLoading()
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire({
+            title: `Agendado`
+          })
+        }
+      })
+}
 
-cliente.nombrePerfil = setProfileName(cliente); */
+function setNextAppointment2(){
+    Swal.fire({
+        title: 'Entrarla fecha en el formato DDMMYYYY',
+        input: 'text',
+        inputAttributes: {
+          autocapitalize: 'off'
+        },
+        showCancelButton: true,
+        confirmButtonText: 'Confirmar',
+        showLoaderOnConfirm: true,
+        preConfirm: (date) => {
+          try{
+            let customer = localStorage.getItem('client');
+            customer.mascotas[2].proximaCita = date;
+            localStorage.setItem('client', customer);
+            console.log(customer)
+          } 
+        catch{
+              Swal.showValidationMessage(
+                `Fallo al agendar`
+              )
+            }
+        },
+        allowOutsideClick: () => !Swal.isLoading()
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire({
+            title: `Agendado`
+          })
+        }
+      })
+}
